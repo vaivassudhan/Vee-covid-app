08063cc3d43ef3f17f526342121a85df2a2de954
ragulhchc <ragulhchc@gmail.com>
2021-05-14 15:52:51 +0530

    commit

--- vee-covid-app/App.js
+++ vee-covid-app/App.js
@@ -1,15 +1,109 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View ,Button , ActivityIndicator} from 'react-native';
+import { NavigationContainer, DefaultTheme as NavigationDefaultTheme,
+  DarkTheme as NavigationDarkTheme } from '@react-navigation/native';
+import { 
+    Provider as PaperProvider, 
+    DefaultTheme as PaperDefaultTheme,
+    DarkTheme as PaperDarkTheme 
+  } from 'react-native-paper';
+import RootStackScreen from './Components/Navigation/RootstackScreen';
 
-export default function App() {
+import { enableScreens } from 'react-native-screens';
+
+const App = () => {
+  enableScreens();
+
+
+  const [isDarkTheme, setIsDarkTheme] = React.useState(false);
+
+  const initialLoginState = {
+    isLoading: true,
+    userName: null,
+    userToken: null,
+  };
+
+  const CustomDefaultTheme = {
+    ...NavigationDefaultTheme,
+    ...PaperDefaultTheme,
+    colors: {
+      ...NavigationDefaultTheme.colors,
+      ...PaperDefaultTheme.colors,
+      background: '#efefef',
+      text: '#000000',
+      text_secondary:'3c3c3c',
+      accent1:'#b2b9ff',
+      accent2:'#eee3df',
+      accent3:'#f6a499',
+      backcard:'#ffffff'
+    }
+  }
+  
+  const CustomDarkTheme = {
+    ...NavigationDarkTheme,
+    ...PaperDarkTheme,
+    colors: {
+      ...NavigationDarkTheme.colors,
+      ...PaperDarkTheme.colors,
+      background: '#1B0536',
+      text: '#ecc1f4',
+      text_secondary:'#fe167c',
+      accent1:'#3D225D',
+      accent2:'#260f41',
+      accent3:'#3ccbf4',
+      backcard:'#3D225D'
+    }
+  }
+
+  const theme = isDarkTheme ? CustomDarkTheme : CustomDefaultTheme;
+
+  const loginReducer = (prevState, action) => {
+    switch( action.type ) {
+      case 'RETRIEVE_TOKEN': 
+        return {
+          ...prevState,
+          userToken: action.token,
+          isLoading: false,
+        };
+      case 'LOGIN': 
+        return {
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+          isLoading: false,
+        };
+      case 'LOGOUT': 
+        return {
+          ...prevState,
+          userName: null,
+          userToken: null,
+          isLoading: false,
+        };
+      case 'REGISTER': 
+        return {
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+          isLoading: false,
+        };
+    }
+  };
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+    <NavigationContainer theme={theme}>
+           <RootStackScreen/>
+
+    </NavigationContainer>
+    </PaperProvider>
+  
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
