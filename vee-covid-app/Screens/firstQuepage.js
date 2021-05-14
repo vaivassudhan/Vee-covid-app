dded31de941aa50e1af6cdac9f35351e4419d67d
ragulhchc <ragulhchc@gmail.com>
2021-05-14 15:52:41 +0530

    commit

---
+++ vee-covid-app/Screens/firstQuepage.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { 
+    SafeAreaView,
+    View, 
+    ScrollView ,
+    Text, 
+    TouchableOpacity, 
+    TextInput,
+    Platform,
+    StyleSheet ,
+    StatusBar,
+    Alert,
+    Image,
+    ImageBackground
+} from 'react-native';
+
+export class firstQuepage extends Component {
+    render() {
+        return (
+            <View>
+            <Text>
+                Hi This is First Page.....
+            </Text>
+            </View>
+        )
+    }
+}
+
+export default firstQuepage
