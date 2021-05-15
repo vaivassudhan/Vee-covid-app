2edf6d45de3a31afdbfd9ebd46a88aea9fe3e13c
ragulhchc <ragulhchc@gmail.com>
2021-05-15 14:27:41 +0530

    symptoms

--- vee-covid-app/Screens/Symptoms.js
+++ vee-covid-app/Screens/Symptoms.js
@@ -15,41 +15,21 @@ import {
 import { Avatar, Button, Card, Title, Paragraph, Divider } from 'react-native-paper';
 import {Checkbox, Assets, Text, View, Colors, Spacings} from 'react-native-ui-lib'; 
 import Icon from 'react-native-vector-icons/Ionicons';
-class Symptoms extends Component {
-  state = {
-    value1: false,
-    value2: false,
-    value3: false,
-    value4: false,
-    value5: false,
-    value6: false,
-    value7: false,
-    value8: false,
-    value9: false,
-    value10: false,
-  };
-  find(){
-    
-    let arr =[]
-    const dat = [ 'Fever' , 'Cold' , 'Loss of smell' , 'Loss of taste' , 'Headache' , 'Pain in chest with deep breaths' 
-    , 'Shortness of breath' , 'Sore Throat' , 'Stomach ache' , 'Diarrhea']
-    if(this.state.value1){arr.push(dat[0])}
-    if(this.state.value2){arr.push(dat[1])}
-    if(this.state.value3){arr.push(dat[2])}
-    if(this.state.value4){arr.push(dat[3])}
-    if(this.state.value5){arr.push(dat[4])}
-    if(this.state.value6){arr.push(dat[5])}
-    if(this.state.value7){arr.push(dat[6])}
-    if(this.state.value8){arr.push(dat[7])}
-    if(this.state.value9){arr.push(dat[8])}
-    if(this.state.value10){arr.push(dat[9])}
-    alert(arr)
-
-    this.props.navigation.navigate('Remedeselect')
-  }
-  render (){
-    const dat = [ 'Fever' , 'Cold' , 'Loss of smell' , 'Loss of taste' , 'Headache' , 'Pain in chest with deep breaths' 
-    , 'Shortness of breath' , 'Sore Throat' , 'Stomach ache' , 'Diarrhea']
+import { TagSelect } from 'react-native-tag-select';
+export default class Sympto extends React.Component{
+    render() {
+        const data = [
+          { id: 1, label: 'Fever' },
+          { id: 2, label: 'Cold' },
+          { id: 3, label: 'Loss of smell' },
+          { id: 4, label: 'Loss of taste' },
+          { id: 5, label: 'Headache' },
+          { id: 6, label: 'Pain in chest with deep breaths' },
+          { id: 7, label: 'Shortness of breath' },
+          { id: 8, label: 'Sore Throat' },
+          { id: 9, label: 'Stomach ache' },
+          { id: 10, label: 'Diarrhea' },
+        ];
         return (
             <View style={styles.home}>
             <View style={styles.innerbox}>
@@ -57,130 +37,25 @@ class Symptoms extends Component {
             <Icon.Button name="chevron-back-sharp" size={40} backgroundColor="#FFFFFF" color="#4446AD"></Icon.Button> 
             </View>
             <StatusBar backgroundColor='#4446AD' barStyle="light-content"/>
-            <Button onPress = {()=>{this.find()}} />
+              
             <Text style={[styles.title]}>Do you have any of these?</Text>
-            <View>
-            <View useSafeArea flex>
-        <View flex padding-20>
-          <View flex center>
-        <View>
-            <View style={{marginTop:30}} flex row>
-            <Checkbox
-              value={this.state.value1}
-              size={30}
-              label={dat[0]}
-              color={Colors.blue10}
-              onValueChange={value1 => this.setState({value1})}
-              containerStyle={{marginBottom: 20, marginLeft:30}}
-            />
-            <Checkbox
-              value={this.state.value2}
-              size={30}
-              label={dat[1]}
-              color={Colors.blue10}
-              onValueChange={value2 => this.setState({value2})}
-              containerStyle={{marginBottom: 20 , marginLeft:'30%'}}
-            />
-            </View>
-            <View style={{marginTop:50}} flex row>
-            <Checkbox
-              value={this.state.value3}
-              size={30}
-              label={dat[2]}
-              color={Colors.blue10}
-              onValueChange={value3 => this.setState({value3})}
-              containerStyle={{marginBottom: 20, marginLeft:30}}
-            />
-            <Checkbox
-              value={this.state.value4}
-              size={30}
-              label={dat[3]}
-              color={Colors.blue10}
-              onValueChange={value4 => this.setState({value4})}
-              containerStyle={{marginBottom: 20 , marginLeft:'15%'}}
-            />
-            </View>
-            <View style={{marginTop:50}}>
-            <Checkbox
-              value={this.state.value5}
-              size={30}
-              label={dat[4]}
-              color={Colors.blue10}
-              onValueChange={value5 => this.setState({value5})}
-              containerStyle={{marginBottom: 20, marginLeft:30}}
-            />
-            </View>
-            <View style={{marginTop:50}}>
-            <Checkbox
-              value={this.state.value6}
-              size={30}
-              label={dat[5]}
-              color={Colors.blue10}
-              onValueChange={value6 => this.setState({value6})}
-              containerStyle={{marginBottom: 20 , marginLeft:30}}
-            />
-            </View>
-
-            <View style={{marginTop:50}}>
-            <Checkbox
-              value={this.state.value7}
-              size={30}
-              label={dat[6]}
-              color={Colors.blue10}
-              onValueChange={value7 => this.setState({value7})}
-              containerStyle={{marginBottom: 20, marginLeft:30}}
-            />
-            </View>
-            <View style={{marginTop:50}}>
-            <Checkbox
-              value={this.state.value8}
-              size={30}
-              label={dat[7]}
-              color={Colors.blue10}
-              onValueChange={value8 => this.setState({value8})}
-              containerStyle={{marginBottom: 20 , marginLeft:30}}
-            />
-            </View>
-
-            <View style={{marginTop:50}} flex row>
-            <Checkbox
-              value={this.state.value9}
-              size={30}
-              label={dat[8]}
-              color={Colors.blue10}
-              onValueChange={value9 => this.setState({value9})}
-              containerStyle={{marginBottom: 20, marginLeft:30}}
-            />
-            
-            <Checkbox
-              value={this.state.value10}
-              size={30}
-              label={dat[9]}
-              color={Colors.blue10}
-              onValueChange={value10 => this.setState({value10})}
-              containerStyle={{marginBottom: 20 , marginLeft:'12%'}}
-            />
-            
-            </View>
-          </View>
-          
-          </View>
-        </View>
-      </View>
-      
+            <View style={{marginTop:"15%",marginLeft:"15%"}}>
+                    <TagSelect
+                      theme="info"
+                        data={data}
+                        itemStyle={styles.customItem}
+                    
+                        ref={(tag) => {
+                              this.tag = tag;
+                        }}
+                      />   
             </View>
             </View>
-            <TouchableOpacity onPress = {()=>{this.find()}}>  
-            <View>     
-            <Icon.Button name="chevron-back-sharp" size={40} backgroundColor="#4446AD" color="#ffffff"></Icon.Button> 
-            </View>
-            </TouchableOpacity>
+            <Text>Progress Bar</Text>
             </View>
         )
     }
-  }
-
-export default Symptoms
+}
 
 const styles = StyleSheet.create({
      title: {    
