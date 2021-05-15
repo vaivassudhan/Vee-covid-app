14c84946e6378e4cd7f5acafaf7edbfbd7ffc024
ragulhchc <ragulhchc@gmail.com>
2021-05-15 12:47:15 +0530

    Style

---
+++ vee-covid-app/Screens/Styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    card: {
+        marginLeft:12,
+            marginRight:12,
+            width: '93%',
+            marginTop:8,
+            marginBottom:2,
+            flexDirection:'row',
+            shadowColor: "#000",
+            shadowOffset: {
+                width: 0,
+                height: 4,
+            },
+            shadowOpacity: 0.30,
+            shadowRadius: 11.95,
+            borderRadius:20,
+    
+            elevation: 8,
+      },  
+});
