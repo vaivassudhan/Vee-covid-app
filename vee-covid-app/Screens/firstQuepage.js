dd292f1928fe359bf0bcf896a772da7e7d07c2af
Shreinik Jain <1029384756hhh@gmail.com>
2021-05-15 13:58:42 +0530

    are you feeling well is themed

--- vee-covid-app/Screens/firstQuepage.js
+++ vee-covid-app/Screens/firstQuepage.js
@@ -16,7 +16,77 @@ import {
 import { Avatar, Button, Card, Title, Paragraph, Divider } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/Ionicons';
 import * as Progress from 'react-native-progress';
+import { useTheme } from '@react-navigation/native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 export function firstQuepage({navigation}){
+  const { colors } = useTheme();
+  const styles = StyleSheet.create({
+    title: {    
+     //color: '#05375a',
+     color: colors.accent1,
+     marginLeft:30,
+     marginTop:20,
+     fontSize: 30,
+     fontWeight: 'bold'
+ },
+ questions: {    
+   //color: '#05375a',
+   color: colors.accent2,
+   marginLeft:90,
+   marginTop:30,
+   fontSize: 20,
+   fontWeight: 'bold'
+},
+   cardbutton:{
+     alignItems:'center',
+     backgroundColor:colors.accent3,
+     height:'50%',
+     width:'35%',
+     marginTop:'5%',
+     margin:25,
+     justifyContent:'center',
+     shadowColor: colors.text,
+     shadowOffset: {
+         width: 0,
+         height: 9,
+     },
+     shadowOpacity: 0.48,
+     shadowRadius: 11.95,
+     borderRadius:20,
+     borderColor:'grey',
+     elevation: 2,
+   },
+   buttontext:{
+     color:'#00a000',
+     fontSize:18,
+     marginTop:12,
+     fontWeight: 'bold'
+   },
+   buttontext2:{
+     color:'#dd3c3c',
+     fontSize:18,
+     marginTop:12,
+     fontWeight: 'bold'
+   },
+   next:{
+     marginLeft:300,
+     width:100,
+     borderRadius:15,
+   },
+   home:{
+     width:"100%",
+     height:"100%",
+     backgroundColor:colors.accent1,
+},
+innerbox:{
+     width:"96%",
+     height:"80%",
+     borderRadius:30,
+     marginHorizontal:"2%",
+     marginTop:"2%",
+     backgroundColor:"white"
+},
+ });
         return (
             // <View style={styles.centeredView}>
             // <StatusBar backgroundColor='#fff' barStyle="light-content"/>
@@ -49,9 +119,9 @@ export function firstQuepage({navigation}){
             <View style={styles.home}>
             <View style={styles.innerbox}>
             <View style={{marginTop:30,marginLeft:10}}>       
-            <Icon.Button name="chevron-back-sharp" size={40} backgroundColor="#FFFFFF" color="#4446AD"></Icon.Button> 
+            <Icon.Button name="chevron-back-sharp" size={40} backgroundColor={colors.backcard} color={colors.accent1}></Icon.Button> 
             </View>
-            <StatusBar backgroundColor='#4446AD' barStyle="light-content"/>
+            <StatusBar backgroundColor={colors.accent1} barStyle="light-content"/>
               
                     <Text style={[styles.title]}>Are You Feeling Good Today?</Text>
                     
@@ -75,10 +145,10 @@ export function firstQuepage({navigation}){
                    </Card.Actions>
             </View>
             <Text style={[styles.questions]} >Progress:</Text>
-              <Progress.Bar progress={0.3} width={250} color='#f4bc1c' borderColor='white' height={10} marginLeft={30} marginTop={10} />
+              <Progress.Bar progress={0.3} width={250} color={colors.accent2} borderColor='white' height={10} marginLeft={30} marginTop={10} />
             <Divider/>
             <Card style={[styles.next]}>
-            <Icon.Button name="chevron-forward-sharp" size={35} backgroundColor="#f4bc1c" color="#4446AD" borderRadius={15}>Next</Icon.Button> 
+            <Icon.Button name="chevron-forward-sharp" size={35} backgroundColor={colors.accent2} color={colors.accent1} borderRadius={15}>Next</Icon.Button> 
             </Card>
             </View>
         )
@@ -86,70 +156,3 @@ export function firstQuepage({navigation}){
 
 export default firstQuepage
 
-const styles = StyleSheet.create({
-     title: {    
-      //color: '#05375a',
-      color: '#4446AD',
-      marginLeft:30,
-      marginTop:20,
-      fontSize: 30,
-      fontWeight: 'bold'
-  },
-  questions: {    
-    //color: '#05375a',
-    color: '#ffffff',
-    marginLeft:90,
-    marginTop:30,
-    fontSize: 20,
-    fontWeight: 'bold'
-},
-    cardbutton:{
-      alignItems:'center',
-      backgroundColor:'white',
-      height:'50%',
-      width:'35%',
-      marginTop:'5%',
-      margin:25,
-      justifyContent:'center',
-      shadowColor: "#000",
-      shadowOffset: {
-          width: 0,
-          height: 9,
-      },
-      shadowOpacity: 0.48,
-      shadowRadius: 11.95,
-      borderRadius:20,
-      borderColor:'grey',
-      elevation: 2,
-    },
-    buttontext:{
-      color:'#008000',
-      fontSize:18,
-      marginTop:12,
-      fontWeight: 'bold'
-    },
-    buttontext2:{
-      color:'#dd3c3c',
-      fontSize:18,
-      marginTop:12,
-      fontWeight: 'bold'
-    },
-    next:{
-      marginLeft:300,
-      width:100,
-      borderRadius:15,
-    },
-    home:{
-      width:"100%",
-      height:"100%",
-      backgroundColor:"#4446AD",
-},
-innerbox:{
-      width:"96%",
-      height:"80%",
-      borderRadius:30,
-      marginHorizontal:"2%",
-      marginTop:"2%",
-      backgroundColor:"white"
-},
-  });
